93ffd78ec204b37379faeaa177f65676e4aab9fa
Korvin Ezüst <dev@korvin.eu>
2018-02-16 00:07:54 +0100

    first working demo

--- randompass_gui.py
+++ randompass_gui.py
@@ -21,13 +21,22 @@ limitations under the License.
 
 import tkinter as tk
 from tkinter import font
+from rndplib.generator import (
+    downloader,
+    words_stopwords,
+    trimmed_words,
+    passphrase,
+    expanded_passphrase,
+    password
+)
+import string
 
 __author__ = "Korvin F. Ezüst"
 __copyright__ = "Copyright (c) 2018, Korvin F. Ezüst"
 __license__ = "Apache 2.0"
-__version__ = "0.1a"
+__version__ = "0.9"
 __email__ = "dev@korvin.eu"
-__status__ = "Preview"
+__status__ = "Demo"
 
 # Root window
 root = tk.Tk()
@@ -114,25 +123,151 @@ nframe = tk.Frame(root)
 nframe.grid(row=8, column=1, pady=10)
 number = tk.Entry(nframe)
 number.grid(row=0, column=1)
+
+
 # Number increment and decrement buttons
-dec = tk.Button(nframe, text="-")
+
+
+def increment():
+    """Increment number by one"""
+    try:
+        # Try casting it to int
+        n = int(number.get())
+        # Make sure it stays positive
+        if n < 1:
+            n = 0
+    except ValueError:
+        n = 0
+    # Increment
+    number.delete("0", tk.END)
+    number.insert("0", str(n + 1))
+
+
+def decrement():
+    """Decrement number by one"""
+    try:
+        # Try casting it to int
+        n = int(number.get())
+        # Make sure it stays positive
+        if n <= 1:
+            n = 2
+    except ValueError:
+        n = 2
+    # Decrement
+    number.delete("0", tk.END)
+    number.insert("0", str(n - 1))
+
+
+dec = tk.Button(nframe, text="-", command=decrement)
 dec.grid(row=0, column=0)
-inc = tk.Button(nframe, text="+")
+inc = tk.Button(nframe, text="+", command=increment)
 inc.grid(row=0, column=2)
 
-# Ok Button
-ok_button = tk.Button(root, text="Generate")
-ok_button.grid(row=9, column=1, pady=20)
-
-# Password or passphrase
-password = tk.StringVar()
 # Output
-password_out = tk.Text(root, height=1, wrap="none", font=fn)
-password_out.grid(row=10, columnspan=2, sticky="WE")
+pass_out = tk.Text(root, height=1, wrap="none", font=fn)
+pass_out.grid(row=10, columnspan=2, sticky="WE")
 pscroll = tk.Scrollbar(root, orient="horizontal")
 pscroll.grid(row=11, columnspan=2, sticky="WE")
-password_out.config(xscrollcommand=pscroll.set)
-pscroll.config(command=password_out.xview)
+pass_out.config(xscrollcommand=pscroll.set)
+pscroll.config(command=pass_out.xview)
+
+
+def generate():
+    """Generate password or passphrase"""
+    global pass_type
+    # Clear text field
+    pass_out.delete("0.0", tk.END)
+    # Get password type and extra option
+    t = pass_type.get()
+    o = option.get()
+
+    if t == "alphanumeric":
+        chars = string.ascii_letters + string.digits
+        if o == "ASCII":
+            chars += string.punctuation
+        elif o == "custom":
+            chars += custom_input.get()
+    elif t == "binary":
+        chars = "01"
+        if o == "ASCII":
+            chars += string.punctuation
+        elif o == "custom":
+            chars += custom_input.get()
+    elif t == "octal":
+        chars = string.octdigits
+        if o == "ASCII":
+            chars += string.punctuation
+        elif o == "custom":
+            chars += custom_input.get()
+    elif t == "decimal":
+        chars = string.digits
+        if o == "ASCII":
+            chars += string.punctuation
+        elif o == "custom":
+            chars += custom_input.get()
+    elif t == "hexadecimal":
+        chars = string.hexdigits
+        if o == "ASCII":
+            chars += string.punctuation
+        elif o == "custom":
+            chars += custom_input.get()
+    else:
+        chars = string.printable.strip()
+        if o == "custom":
+            chars += custom_input.get()
+    # Convert number to int or print error message and set it to zero
+    try:
+        n = int(number.get())
+        if n <= 0:
+            pass_out.insert("0.0", "INVALID NUMBER")
+    except ValueError:
+        pass_out.insert("0.0", "INVALID NUMBER")
+        n = 0
+
+    # Type is password
+    if t != "words":
+        # If n <= zero, there's nothing to do
+        if n > 0:
+            # Print password
+            pass_out.insert("0.0", password(chars, n))
+
+    # Type is passphrase
+    else:
+        # Get word list
+        try:
+            word_list, stop_word_list = words_stopwords()
+            word_list = trimmed_words(word_list, stop_word_list)
+        except LookupError:
+            # TODO: make this message appear before download
+            pass_out.insert("0.0", "DOWNLOADING WORD LIST...")
+            downloader()
+            pass_out.delete("0.0", tk.END)
+            word_list, stop_word_list = words_stopwords()
+            word_list = trimmed_words(word_list, stop_word_list)
+
+        # Set value of ext based on selected extra option
+        if o == "ASCII":
+            ext = string.punctuation
+        elif o == "custom":
+            ext = custom_input.get()
+        else:
+            ext = ""
+        # The function expanded_passphrase needs a list
+        ext = [i for i in ext]
+        # Get passphrase
+        p = passphrase(word_list, n)
+        # If n == zero, nothing to do
+        if n > 0:
+            # Expand passphrase with characters from ext if any
+            if len(ext) > 0:
+                p = expanded_passphrase(p, n, ext)
+            # Print passphrase
+            pass_out.insert("0.0", p)
+
+
+# Generate Button
+generate_button = tk.Button(root, text="Generate", command=generate)
+generate_button.grid(row=9, column=1, pady=20)
 
 # Exit Button
 exit_button = tk.Button(root, text="Exit", command=root.destroy)
