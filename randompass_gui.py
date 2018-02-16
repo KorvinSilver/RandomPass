512694b67b4330f844f919de6140c55fa17e28fe
Korvin Ezüst <dev@korvin.eu>
2018-02-16 20:25:56 +0100

    minor formatting and comment changes

--- randompass_gui.py
+++ randompass_gui.py
@@ -68,10 +68,14 @@ class AboutWindow(BasicWindow):
         super(AboutWindow, self).__init__()
         self.title("About")
 
+        # Set bigger font
         fn = font.Font(size=14)
+
+        # Title label
         label = tk.Label(
             self, text=f"RandomPass version {__version__}", font=fn)
         label.grid(row=0, columnspan=2, pady=20, padx=10)
+        # Label with program description
         label2 = tk.Label(
             self,
             text="A program to create various types of passwords and "
@@ -79,23 +83,28 @@ class AboutWindow(BasicWindow):
         label2.grid(row=1, columnspan=2, padx=10, pady=10)
 
         def callback_python(event):
+            """Open web browser to python.org"""
             webbrowser.open_new("https://www.python.org")
 
+        # Label link to python.org
         label3 = tk.Label(
             self, text="Written in Python", fg="blue", cursor="hand2")
         label3.bind("<Button-1>", callback_python)
         label3.grid(row=2, columnspan=2, padx=10, pady=10)
 
         def callback_source(event):
+            """Open web browser to source code"""
             webbrowser.open_new("https://gitlab.com/KorvinSilver/RandomPass")
 
+        # Label link to source code
         label4 = tk.Label(
             self, text="View source code", fg="blue", cursor="hand2")
         label4.bind("<Button-1>", callback_source)
         label4.grid(row=3, columnspan=2, padx=10, pady=10)
 
-        exit_button = tk.Button(self, text="Close", command=self.destroy)
-        exit_button.grid(row=4, columnspan=2, pady=20)
+        # Close button
+        close_button = tk.Button(self, text="Close", command=self.destroy)
+        close_button.grid(row=4, columnspan=2, pady=20)
 
 
 class DocWindow(BasicWindow):
@@ -107,35 +116,49 @@ class LicenseWindow(BasicWindow):
         super(LicenseWindow, self).__init__()
         self.title("License")
 
+        # Group text field and scrollbar into a frame
         frame = tk.Frame(self)
         frame.grid(row=0, columnspan=2)
+        # Text field, 80 characters wide
         text = tk.Text(frame, width=80)
         text.grid(row=0, column=0)
+        # Scrollbar
         scroll = tk.Scrollbar(frame, orient="vertical")
         scroll.grid(row=0, column=1, sticky="NS")
+        # Connect text field and scrollbar
         text.config(yscrollcommand=scroll.set)
         scroll.config(command=text.yview)
+
+        # Full license text
         license_text = \
-            "\n                                 Apache License\n             "\
-            "              Version 2.0, January 2004\n                       "\
-            " http://www.apache.org/licenses/\n\n   TERMS AND CONDITIONS FOR "\
-            "USE, REPRODUCTION, AND DISTRIBUTION\n\n   1. Definitions.\n\n   "\
+            "\n                                 Apache License\n            " \
+            " " \
+            "              Version 2.0, January 2004\n                      " \
+            " " \
+            " http://www.apache.org/licenses/\n\n   TERMS AND CONDITIONS FOR" \
+            " " \
+            "USE, REPRODUCTION, AND DISTRIBUTION\n\n   1. Definitions.\n\n  " \
+            " " \
             "   \"License\" shall mean the terms and conditions for use, " \
             "reproduction,\n      and distribution as defined by Sections 1 " \
             "through 9 of this document.\n\n      \"Licensor\" shall mean " \
             "the copyright owner or entity authorized by\n      the " \
             "copyright owner that is granting the License.\n\n      \"Legal " \
-            "Entity\" shall mean the union of the acting entity and all\n    "\
+            "Entity\" shall mean the union of the acting entity and all\n   " \
+            " " \
             "  other entities that control, are controlled by, or are under " \
             "common\n      control with that entity. For the purposes of " \
             "this definition,\n      \"control\" means (i) the power, " \
-            "direct or indirect, to cause the\n      direction or management "\
+            "direct or indirect, to cause the\n      direction or management" \
+            " " \
             "of such entity, whether by contract or\n      otherwise, " \
             "or (ii) ownership of fifty percent (50%) or more of the\n      " \
             "outstanding shares, or (iii) beneficial ownership of such " \
-            "entity.\n\n      \"You\" (or \"Your\") shall mean an individual "\
+            "entity.\n\n      \"You\" (or \"Your\") shall mean an individual" \
+            " " \
             "or Legal Entity\n      exercising permissions granted by this " \
-            "License.\n\n      \"Source\" form shall mean the preferred form "\
+            "License.\n\n      \"Source\" form shall mean the preferred form" \
+            " " \
             "for making modifications,\n      including but not limited to " \
             "software source code, documentation\n      source, " \
             "and configuration files.\n\n      \"Object\" form shall mean " \
@@ -144,26 +167,33 @@ class LicenseWindow(BasicWindow):
             "to compiled object code, generated documentation,\n      and " \
             "conversions to other media types.\n\n      \"Work\" shall mean " \
             "the work of authorship, whether in Source or\n      Object " \
-            "form, made available under the License, as indicated by a\n     "\
-            " copyright notice that is included in or attached to the work\n "\
+            "form, made available under the License, as indicated by a\n    " \
+            " " \
+            " copyright notice that is included in or attached to the work\n" \
+            " " \
             "     (an example is provided in the Appendix below).\n\n      " \
             "\"Derivative Works\" shall mean any work, whether in Source or " \
-            "Object\n      form, that is based on (or derived from) the Work "\
+            "Object\n      form, that is based on (or derived from) the Work" \
+            " " \
             "and for which the\n      editorial revisions, annotations, " \
             "elaborations, or other modifications\n      represent, " \
-            "as a whole, an original work of authorship. For the purposes\n  "\
+            "as a whole, an original work of authorship. For the purposes\n " \
+            " " \
             "    of this License, Derivative Works shall not include works " \
             "that remain\n      separable from, or merely link (or bind by " \
             "name) to the interfaces of,\n      the Work and Derivative " \
-            "Works thereof.\n\n      \"Contribution\" shall mean any work of "\
+            "Works thereof.\n\n      \"Contribution\" shall mean any work of" \
+            " " \
             "authorship, including\n      the original version of the Work " \
             "and any modifications or additions\n      to that Work or " \
             "Derivative Works thereof, that is intentionally\n      " \
             "submitted to Licensor for inclusion in the Work by the " \
             "copyright owner\n      or by an individual or Legal Entity " \
             "authorized to submit on behalf of\n      the copyright owner. " \
-            "For the purposes of this definition, \"submitted\"\n      means "\
-            "any form of electronic, verbal, or written communication sent\n "\
+            "For the purposes of this definition, \"submitted\"\n      means" \
+            " " \
+            "any form of electronic, verbal, or written communication sent\n" \
+            " " \
             "     to the Licensor or its representatives, including but not " \
             "limited to\n      communication on electronic mailing lists, " \
             "source code control systems,\n      and issue tracking systems " \
@@ -172,18 +202,24 @@ class LicenseWindow(BasicWindow):
             "excluding communication that is conspicuously marked or " \
             "otherwise\n      designated in writing by the copyright owner " \
             "as \"Not a Contribution.\"\n\n      \"Contributor\" shall mean " \
-            "Licensor and any individual or Legal Entity\n      on behalf of "\
+            "Licensor and any individual or Legal Entity\n      on behalf of" \
+            " " \
             "whom a Contribution has been received by Licensor and\n      " \
             "subsequently incorporated within the Work.\n\n   2. Grant of " \
-            "Copyright License. Subject to the terms and conditions of\n     "\
+            "Copyright License. Subject to the terms and conditions of\n    " \
+            " " \
             " this License, each Contributor hereby grants to You a " \
             "perpetual,\n      worldwide, non-exclusive, no-charge, " \
             "royalty-free, irrevocable\n      copyright license to " \
             "reproduce, prepare Derivative Works of,\n      publicly " \
-            "display, publicly perform, sublicense, and distribute the\n     "\
-            " Work and such Derivative Works in Source or Object form.\n\n   "\
-            "3. Grant of Patent License. Subject to the terms and conditions "\
-            "of\n      this License, each Contributor hereby grants to You a "\
+            "display, publicly perform, sublicense, and distribute the\n    " \
+            " " \
+            " Work and such Derivative Works in Source or Object form.\n\n  " \
+            " " \
+            "3. Grant of Patent License. Subject to the terms and conditions" \
+            " " \
+            "of\n      this License, each Contributor hereby grants to You a" \
+            " " \
             "perpetual,\n      worldwide, non-exclusive, no-charge, " \
             "royalty-free, irrevocable\n      (except as stated in this " \
             "section) patent license to make, have made,\n      use, " \
@@ -193,7 +229,8 @@ class LicenseWindow(BasicWindow):
             "infringed by their\n      Contribution(s) alone or by " \
             "combination of their Contribution(s)\n      with the Work to " \
             "which such Contribution(s) was submitted. If You\n      " \
-            "institute patent litigation against any entity (including a\n   "\
+            "institute patent litigation against any entity (including a\n  " \
+            " " \
             "   cross-claim or counterclaim in a lawsuit) alleging that the " \
             "Work\n      or a Contribution incorporated within the Work " \
             "constitutes direct\n      or contributory patent infringement, " \
@@ -203,28 +240,35 @@ class LicenseWindow(BasicWindow):
             "reproduce and distribute copies of the\n      Work or " \
             "Derivative Works thereof in any medium, with or without\n      " \
             "modifications, and in Source or Object form, provided that " \
-            "You\n      meet the following conditions:\n\n      (a) You must "\
+            "You\n      meet the following conditions:\n\n      (a) You must" \
+            " " \
             "give any other recipients of the Work or\n          Derivative " \
             "Works a copy of this License; and\n\n      (b) You must cause " \
             "any modified files to carry prominent notices\n          " \
             "stating that You changed the files; and\n\n      (c) You must " \
             "retain, in the Source form of any Derivative Works\n          " \
-            "that You distribute, all copyright, patent, trademark, and\n    "\
+            "that You distribute, all copyright, patent, trademark, and\n   " \
+            " " \
             "      attribution notices from the Source form of the Work," \
             "\n          excluding those notices that do not pertain to any " \
             "part of\n          the Derivative Works; and\n\n      (d) If " \
-            "the Work includes a \"NOTICE\" text file as part of its\n       "\
+            "the Work includes a \"NOTICE\" text file as part of its\n      " \
+            " " \
             "   distribution, then any Derivative Works that You distribute " \
             "must\n          include a readable copy of the attribution " \
-            "notices contained\n          within such NOTICE file, excluding "\
-            "those notices that do not\n          pertain to any part of the "\
+            "notices contained\n          within such NOTICE file, excluding" \
+            " " \
+            "those notices that do not\n          pertain to any part of the" \
+            " " \
             "Derivative Works, in at least one\n          of the following " \
             "places: within a NOTICE text file distributed\n          as " \
-            "part of the Derivative Works; within the Source form or\n       "\
+            "part of the Derivative Works; within the Source form or\n      " \
+            " " \
             "   documentation, if provided along with the Derivative Works; " \
             "or,\n          within a display generated by the Derivative " \
             "Works, if and\n          wherever such third-party notices " \
-            "normally appear. The contents\n          of the NOTICE file are "\
+            "normally appear. The contents\n          of the NOTICE file are" \
+            " " \
             "for informational purposes only and\n          do not modify " \
             "the License. You may add Your own attribution\n          " \
             "notices within Derivative Works that You distribute, " \
@@ -237,26 +281,32 @@ class LicenseWindow(BasicWindow):
             "reproduction, or distribution of Your modifications, or\n      " \
             "for any such Derivative Works as a whole, provided Your use," \
             "\n      reproduction, and distribution of the Work otherwise " \
-            "complies with\n      the conditions stated in this License.\n\n "\
+            "complies with\n      the conditions stated in this License.\n\n" \
+            " " \
             "  5. Submission of Contributions. Unless You explicitly state " \
             "otherwise,\n      any Contribution intentionally submitted for " \
             "inclusion in the Work\n      by You to the Licensor shall be " \
             "under the terms and conditions of\n      this License, without " \
             "any additional terms or conditions.\n      Notwithstanding the " \
             "above, nothing herein shall supersede or modify\n      the " \
-            "terms of any separate license agreement you may have executed\n "\
+            "terms of any separate license agreement you may have executed\n" \
+            " " \
             "     with Licensor regarding such Contributions.\n\n   6. " \
             "Trademarks. This License does not grant permission to use the " \
-            "trade\n      names, trademarks, service marks, or product names "\
+            "trade\n      names, trademarks, service marks, or product names" \
+            " " \
             "of the Licensor,\n      except as required for reasonable and " \
             "customary use in describing the\n      origin of the Work and " \
-            "reproducing the content of the NOTICE file.\n\n   7. Disclaimer "\
-            "of Warranty. Unless required by applicable law or\n      agreed "\
+            "reproducing the content of the NOTICE file.\n\n   7. Disclaimer" \
+            " " \
+            "of Warranty. Unless required by applicable law or\n      agreed" \
+            " " \
             "to in writing, Licensor provides the Work (and each\n      " \
             "Contributor provides its Contributions) on an \"AS IS\" BASIS," \
             "\n      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either " \
             "express or\n      implied, including, without limitation, " \
-            "any warranties or conditions\n      of TITLE, NON-INFRINGEMENT, "\
+            "any warranties or conditions\n      of TITLE, NON-INFRINGEMENT," \
+            " " \
             "MERCHANTABILITY, or FITNESS FOR A\n      PARTICULAR PURPOSE. " \
             "You are solely responsible for determining the\n      " \
             "appropriateness of using or redistributing the Work and assume " \
@@ -264,7 +314,8 @@ class LicenseWindow(BasicWindow):
             "under this License.\n\n   8. Limitation of Liability. In no " \
             "event and under no legal theory,\n      whether in tort (" \
             "including negligence), contract, or otherwise,\n      unless " \
-            "required by applicable law (such as deliberate and grossly\n    "\
+            "required by applicable law (such as deliberate and grossly\n   " \
+            " " \
             "  negligent acts) or agreed to in writing, shall any " \
             "Contributor be\n      liable to You for damages, including any " \
             "direct, indirect, special,\n      incidental, or consequential " \
@@ -276,21 +327,26 @@ class LicenseWindow(BasicWindow):
             "such Contributor\n      has been advised of the possibility of " \
             "such damages.\n\n   9. Accepting Warranty or Additional " \
             "Liability. While redistributing\n      the Work or Derivative " \
-            "Works thereof, You may choose to offer,\n      and charge a fee "\
+            "Works thereof, You may choose to offer,\n      and charge a fee" \
+            " " \
             "for, acceptance of support, warranty, indemnity,\n      or " \
             "other liability obligations and/or rights consistent with " \
             "this\n      License. However, in accepting such obligations, " \
             "You may act only\n      on Your own behalf and on Your sole " \
             "responsibility, not on behalf\n      of any other Contributor, " \
             "and only if You agree to indemnify,\n      defend, and hold " \
-            "each Contributor harmless for any liability\n      incurred by, "\
+            "each Contributor harmless for any liability\n      incurred by," \
+            " " \
             "or claims asserted against, such Contributor by reason\n      " \
             "of your accepting any such warranty or additional " \
             "liability.\n\n   END OF TERMS AND CONDITIONS\n"
+        # Display license in text field and disable it
         text.insert("0.0", license_text)
         text.config(state="disabled")
-        exit_button = tk.Button(self, text="Close", command=self.destroy)
-        exit_button.grid(row=1, columnspan=2, pady=10)
+
+        # Close button
+        close_button = tk.Button(self, text="Close", command=self.destroy)
+        close_button.grid(row=1, columnspan=2, pady=10)
 
 
 class MainWindow(BasicWindow):
